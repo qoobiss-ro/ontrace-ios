04db20e419f960096a6d18665b3995ba7d30ffdc
Vlad Buhaescu <vlad.buhaescu@qoobiss.com>
2024-05-27 12:43:29 +0300

    update sdk

---
+++ QoobissSDK/Classes/ReplaceMe.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+public class ReplaceMe {
+    
+    public init() {
+        
+    }
+    
+    func aaaa() {
+        let a = UIView()
+//        let aa = FirstClassA()
+//        let aaa = FirstClassB()
+//        IdentitySDKVersionNumber
+    }
+}
